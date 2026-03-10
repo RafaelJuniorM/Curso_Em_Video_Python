48a499282407121bba336fe6f7e44c511d6261d8
Rafael Junior Martins <rafael.junior15@hotmail.com>
2026-03-09 21:47:04 -0300

    [FEAT] Exercicios 69 e 70 - Tuplas

--- Exercicios/ex069.py
+++ Exercicios/ex069.py
@@ -1,2 +1,9 @@
 # Crie um programa que vai gerar cinco numero aleatórios e colocar em uma tupla. 
 # Depois disso, mostre a listagem de números gerados e também indique o menor e o maior valor que estão na tupla.
+
+from random import randint
+
+numero= (randint(0,10), randint(0,10),randint(0,10),randint(0,10),randint(0,10))
+print(f'Os números sorteados foram: {numero}')
+print(f'O maior valor sorteado foi: {max(numero)}')
+print(f'O menor valor sorteado foi: {min(numero)}') 
--- Exercicios/ex070.py
+++ Exercicios/ex070.py
@@ -2,3 +2,16 @@
 # a) Quantas vezes apareceu o valor 9.
 # b) Em que posição foi digitado o primeiro valor 3.
 # c) Quais foram os números pares.
+
+numero = ((int(input('Digite um numero:'))), (int(input('Digite um numero:'))),(int(input('Digite um numero:'))),(int(input('Digite um numero:'))))
+
+print(f'Os números digitados foram: {numero}')
+print(f"O numero 9 apareceu {numero.count(9)} vezes")
+
+if 3 in numero:
+    print(f"o numero 3 apareceu na posição {numero.index(3)}")
+
+for elements in numero:
+    if elements % 2 == 0:
+        print(f'Os números pares foram: {elements}')
+
